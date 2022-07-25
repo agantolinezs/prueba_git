34b61c0dd851c24dd2ee438d72284aa456cfe7d4
agantolinezs <angie.antolinez@mercadolibre.com.co>
2022-07-25 18:29:10 -0500

    Se cree completado.

--- practicaJS.js
+++ practicaJS.js
@@ -1,14 +1,16 @@
-function calculetPrice(name,price){
-    let finalPrice,shippingCost ;
-    if (1<=price<=2000) {
-        shippingCost=300;
+function calculatePrice(name,price){
+    let finalPrice = 0;
+    let shippingCost = 0;
+    if (1 <= price && price <= 2000) {
+        shippingCost = 300;
     }
-    if (2001<=price<=4000) {
-        shippingCost=500;
+    if (2001 <= price && price <= 4000) {
+        shippingCost = 500;
     }
-    if (4000<price) {
-        shippingCost=700;
+    if (4000 < price) {
+        shippingCost = 700;
     }
+    finalPrice = price + shippingCost;
     console.log('El producto '+name+' cuesta '+price+'. Su costo de envío es de '+shippingCost+'. Por lo tanto, el precio final es de '+finalPrice);
 };
 
